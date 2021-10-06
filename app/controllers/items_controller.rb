7491b23c9db28c4d495d20bb687be04c2d62e3df
numair.majid123 <numair.majid@devsinc.com>
2021-10-06 18:35:28 +0500

    Changes apply as sugggested * Modify code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -59,7 +59,6 @@ class ItemsController < ApplicationController
 
   def delete_association
     if (@category = Category.find(params[:format].to_i)) && (@item = @category.items.find(params[:item_id].to_i))
-
       @category.items.delete(@item)
     end
     redirect_back(fallback_location: root_path)
